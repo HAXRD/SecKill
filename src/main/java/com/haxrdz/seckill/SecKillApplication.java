25d1f5a6e087561b3406f1fe4d4490a85e1efe91
HAXRD <xxu.chen@outlook.com>
2022-08-08 21:28:52 +0800

    项目创建并测试可运行

--- src/main/java/com/haxrdz/seckill/SecKillApplication.java
+++ src/main/java/com/haxrdz/seckill/SecKillApplication.java
@@ -1,9 +1,11 @@
 package com.haxrdz.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.haxrdz.seckill.pojo")
 public class SecKillApplication {
 
     public static void main(String[] args) {
