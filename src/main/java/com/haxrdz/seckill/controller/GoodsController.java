94fe0f76267c18a507a81abf39b2ce27befbd5fa
HAXRD <xxu.chen@outlook.com>
2022-08-15 10:24:02 +0800

    商品列表跳转实现

---
+++ src/main/java/com/haxrdz/seckill/controller/GoodsController.java
@@ -0,0 +1,35 @@
+package com.haxrdz.seckill.controller;
+
+import com.haxrdz.seckill.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+
+    /**
+     * 跳转到商品列表页面
+     * @param session
+     * @param model
+     * @param ticket
+     * @return
+     */
+    @RequestMapping("/toList")
+    public String toList(HttpSession session, Model model, @CookieValue("userTicket") String ticket) {
+        if (StringUtils.isEmpty(ticket)) {
+            return "login";
+        }
+        User user = (User) session.getAttribute(ticket);
+        if (user == null) {
+            return "login";
+        }
+        model.addAttribute("user", user);
+        return "goodsList";
+    }
+}
