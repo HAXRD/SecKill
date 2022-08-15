fffcb21c2ef13eb2676abe5b0b6c4ef43dc757ae
HAXRD <xxu.chen@outlook.com>
2022-08-15 16:26:26 +0800

    方案2：通过redis直接存储user信息

--- src/main/java/com/haxrdz/seckill/controller/GoodsController.java
+++ src/main/java/com/haxrdz/seckill/controller/GoodsController.java
@@ -1,31 +1,41 @@
 package com.haxrdz.seckill.controller;
 
 import com.haxrdz.seckill.pojo.User;
+import com.haxrdz.seckill.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
 
+    @Autowired
+    private IUserService userService;
+
     /**
-     * 跳转到商品列表页面
-     * @param session
+     * 获取商品列表
+     * @param request
+     * @param response
      * @param model
      * @param ticket
      * @return
      */
     @RequestMapping("/toList")
-    public String toList(HttpSession session, Model model, @CookieValue("userTicket") String ticket) {
+//    public String toList(HttpSession session, Model model, @CookieValue("userTicket") String ticket) {
+    public String toList(HttpServletRequest request, HttpServletResponse response, Model model, @CookieValue("userTicket") String ticket) {
         if (StringUtils.isEmpty(ticket)) {
             return "login";
         }
-        User user = (User) session.getAttribute(ticket);
+//        User user = (User) session.getAttribute(ticket);
+        User user = userService.getUserByCookie(ticket, request, response);
         if (user == null) {
             return "login";
         }
